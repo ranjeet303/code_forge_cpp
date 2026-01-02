264385bfe0bb96c4d68c88369d9525d11c6a370f
ranjeet303 <rj3032547@gmail.com>
2026-01-02 18:28:24 +0530

    jan2

---
+++ pattern2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void pattern2(int n, char ch);
+int main()
+{
+    char ch1;
+    cout << "enter the character";
+    cin >> ch1;
+    pattern2(5, ch1);
+
+    return 0;
+}
+void pattern2(int n, char ch)
+{
+    for (int i = 0; i < 5; i++)
+    {
+
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << ch;
+        }
+        cout << "\n";
+    }
+}
